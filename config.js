f107f9aab6851811dfc7ef94c16fd3ce38c34b2a
bnnne <yuelianle@gmail.com>
2025-02-07 20:46:50 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+const categories = {
+    purple: {
+        name: "unstackable items",
+        words: ["potion of fire resistance", "bed", "elytra", "shulker box"],
+        color: "purple",
+        solved: false
+    },
+    blue: {
+        name: "piglin bartering experience",
+        words: ["piglin", "gold ingot", "obsidian", "ender pearls"],
+        color: "blue",
+        solved: false
+    },
+    green: {
+        name: "can be fed into a beacon",
+        words: ["iron ingot", "netherite ingot", "diamond", "emerald"],
+        color: "green",
+        solved: false
+    },
+    yellow: {
+        name: "biomes that cant rain",
+        words: ["desert", "savanna", "nether", "the end"],
+        color: "yellow",
+        solved: false
+    }
+};

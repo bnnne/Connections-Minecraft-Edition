5798b150a307a7ec96719547027310e5b8bdeb06
bnnne <yuelianle@gmail.com>
2025-02-08 17:51:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 const categories = {
     purple: {
-        name: "items with a purple name",
-        words: ["dragon head", "elytra", "mace", "silence armor trim"],
+        name: "unstackable items",
+        words: ["bed", "elytra", "shulker box", "potion of fire resistance"],
         color: "purple",
         solved: false
     },
     blue: {
-        name: "items with a blue name",
-        words: ["skull charge banner pattern", "wither skeleton skull", "music disc - pigstep", "vex armor trim"],
+        name: "piglin bartering experience",
+        words: ["piglin", "gold", "ender pearls", "obsidian"],
         color: "blue",
         solved: false
     },
     green: {
-        name: "items with a yellow name",
-        words: ["totem of undying", "skeleton skull", "music disc - cat", "dragon's breath"],
+        name: "can be fed into a beacon",
+        words: ["netherite ingot", "iron ingot", "diamond", "emerald"],
         color: "green",
         solved: false
     },
     yellow: {
-        name: "items with a white name",
-        words: ["cat spawn egg", "raw cod", "string", "flower charge banner pattern"],
+        name: "biomes/dimensions that can't rain",
+        words: ["desert", "savanna", "the nether", "the end"],
         color: "yellow",
         solved: false
     }
